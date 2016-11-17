e2c2d8f5e33a3b8b5b087c3e311eef0b640ed026
liuzihui531 <289747370@qq.com>
2016-11-17 21:44:15 +0800

    a
    a

--- protected/modules/admin/controllers/UserController.php
+++ protected/modules/admin/controllers/UserController.php
@@ -5,6 +5,7 @@
 			$this->breadcrumbs = array($this->page_name.'管理');
 			$email = Yii::app()->request->getParam("email","");
 			$mobile = Yii::app()->request->getParam("mobile","");
+			$group = Yii::app()->request->getParam("group",-1);
 			$criteria = new CDbCriteria();
 			if($email){
 				$criteria->addSearchCondition("email",$email);
@@ -12,10 +13,18 @@
 			if($mobile){
 				$criteria->addSearchCondition("mobile",$mobile);
 			}
+			if($group > 0){
+				$criteria->addCondition("`group`=$group");
+			}
 	        $pager = new CPagination(User::model()->count($criteria));
             $pager->pageSize = 30;
             $pager->applyLimit($criteria);
 	        $model = User::model()->findAll($criteria);
 	        $this->render("index",array('model' => $model, 'pager' => $pager));
+		}
+
+		public function actionCreate(){
+			$model = new User();
+			$this->render("_form",array('model' => $model));
 		}
 	}
---
+++ protected/modules/admin/views/user/_form.php
@@ -0,0 +1,41 @@
+<?php
+$form = $this->beginWidget('CActiveForm', array(
+    'id' => 'id-form',
+    'action' => $this->createUrl('save'),
+    'htmlOptions' => array(
+        'class' => 'form-horizontal',
+        'role' => 'form',
+    ),
+        ));
+?>
+<?php if ($model->id): ?>
+    <input type="hidden" name="id" value="<?php echo $model->id ?>" />
+<?php endif; ?>
+<div class="form-group">
+    <?php echo $form->labelEx($model, 'mobile', array('class' => 'col-sm-3 control-label no-padding-right')) ?>
+
+    <div class="col-sm-9">
+        <?php echo $form->textField($model, 'mobile', array('class' => 'col-xs-10 col-sm-5', 'placeholder' => '')) ?>
+    </div>
+</div>
+<div class="form-group">
+    <?php echo $form->labelEx($model, 'password', array('class' => 'col-sm-3 control-label no-padding-right')) ?>
+
+    <div class="col-sm-9">
+        <?php echo $form->textField($model, 'password', array('class' => 'col-xs-10 col-sm-5', 'placeholder' => '')) ?>
+    </div>
+</div>
+<div class="form-group">
+    <?php echo $form->labelEx($model, 'email', array('class' => 'col-sm-3 control-label no-padding-right')) ?>
+
+    <div class="col-sm-9">
+        <?php echo $form->textField($model, 'email', array('class' => 'col-xs-10 col-sm-5', 'placeholder' => '')) ?>
+    </div>
+</div>
+<div class="form-group">
+    <?php echo $form->labelEx($model, 'realname', array('class' => 'col-sm-3 control-label no-padding-right')) ?>
+
+    <div class="col-sm-9">
+        <?php echo $form->textField($model, 'realname', array('class' => 'col-xs-10 col-sm-5', 'placeholder' => '')) ?>
+    </div>
+</div>
